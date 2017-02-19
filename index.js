15668522e87daa32f77dd1540c7ca90aa503e745
Eyuel Tessema <eyueltessema@gmail.com>
2017-02-19 00:13:28 -0800

    Enable email verification

--- index.js
+++ index.js
@@ -11,19 +11,32 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
+var appName = 'You and Me Forever';
+var serverUrl = process.env.SERVER_URL || 'http://localhost:1337/parse';
+
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  masterKey: process.env.MASTER_KEY || 'masterKey',
+  serverURL: serverUrl,
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  },
+  appName: appName,  // Required for sending email verifications
+  publicServerURL: serverUrl,  // Required for sending email verifications
+
+  verifyUserEmails: true,
+  emailAdapter: {
+    module: 'parse-server-simple-mailgun-adapter',
+    options: {
+      fromAddress: appName + ' <' + process.env.MAILGUN_FROM_ADDRESS + '>',
+      domain: process.env.MAILGUN_DOMAIN,
+      apiKey: process.env.MAILGUN_API_KEY,
+    }
   }
 });
-// Client-keys like the javascript key or the .NET key are not necessary with parse-server
-// If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
+
 
 var app = express();
 
